5ff599a0af09399478f701e93d43dfad0e86557a
Eduardo Marinho <59973014+eduardobivis@users.noreply.github.com>
2022-05-09 17:19:14 -0300

    Fix Identação

--- calculoTempoUtil.php
+++ calculoTempoUtil.php
@@ -1,37 +1,36 @@
 function calculoTempoUtil(DateTime $inicio, DateTime $termino, $horarioInicial, $horarioTermino) {
 
-		########
-		# Dias #
-		########
-		$diasUteis = [1, 2, 3, 4, 5];
-		$feriados = [
-			'*-01-01',
-			'*-12-25', 
-		]; 
+	########
+	# Dias #
+	########
+	$diasUteis = [1, 2, 3, 4, 5];
+	$feriados = [
+		'*-01-01',
+		'*-12-25', 
+	]; 
 
-		//$termino->modify('+1 day');
-		$interval = new DateInterval('P1D');
-		$periods = new DatePeriod($inicio, $interval, $termino);
+	$interval = new DateInterval('P1D');
+	$periods = new DatePeriod($inicio, $interval, $termino);
 
-		$dias = 0;
-		foreach ($periods as $period) {
-			if (!in_array($period->format('N'), $diasUteis)) continue;
-			if (in_array($period->format('*-m-d'), $feriados)) continue;
-			$dias++;
-		}
-
-		//Horas
-		$terminoInicio = clone $inicio;
-		$terminoInicio->modify($horarioTermino);
-		$horasInicio = $inicio->diff($terminoInicio);
-
-		$terminoTermino = clone $termino;
-		$terminoTermino->modify($horarioInicial);
-		$horasTermino = $termino->diff($terminoTermino);
-
-		$totalDias = ( $dias -1 ) + $horasInicio->d + $horasTermino->d;
-		$totalHoras = $horasInicio->h + $horasTermino->h;
-		$totalMinutos = $horasInicio->i + $horasTermino->i;
-
-		return "{$totalDias} dias, {$totalHoras} Horas e {$totalMinutos} Minutos";
+	$dias = 0;
+	foreach ($periods as $period) {
+		if (!in_array($period->format('N'), $diasUteis)) continue;
+		if (in_array($period->format('*-m-d'), $feriados)) continue;
+		$dias++;
 	}
+
+	//Horas
+	$terminoInicio = clone $inicio;
+	$terminoInicio->modify($horarioTermino);
+	$horasInicio = $inicio->diff($terminoInicio);
+
+	$terminoTermino = clone $termino;
+	$terminoTermino->modify($horarioInicial);
+	$horasTermino = $termino->diff($terminoTermino);
+
+	$totalDias = ( $dias -1 ) + $horasInicio->d + $horasTermino->d;
+	$totalHoras = $horasInicio->h + $horasTermino->h;
+	$totalMinutos = $horasInicio->i + $horasTermino->i;
+
+	return "{$totalDias} dias, {$totalHoras} Horas e {$totalMinutos} Minutos";
+}
