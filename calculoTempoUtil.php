4181bb51936101503de13c52fb0807180ddbe11a
Eduardo Marinho <59973014+eduardobivis@users.noreply.github.com>
2022-05-11 09:59:05 -0300

    Atualizando Função

--- calculoTempoUtil.php
+++ calculoTempoUtil.php
@@ -1,4 +1,5 @@
-function calculoTempoUtil(DateTime $inicio, DateTime $termino, $horarioInicial, $horarioTermino) {
+//Tempo entre duas Datas e Horários contando apenas as horas de trabalho
+function calculoTempoUtil(DateTime $inicio, DateTime $termino, $horarioInicial, $horarioTermino) { 
 
 	########
 	# Dias #
@@ -12,25 +13,79 @@ function calculoTempoUtil(DateTime $inicio, DateTime $termino, $horarioInicial, 
 	$interval = new DateInterval('P1D');
 	$periods = new DatePeriod($inicio, $interval, $termino);
 
-	$dias = 0;
+	$totalDias = -1;
 	foreach ($periods as $period) {
 		if (!in_array($period->format('N'), $diasUteis)) continue;
 		if (in_array($period->format('*-m-d'), $feriados)) continue;
-		$dias++;
+		$totalDias++;
 	}
 
-	//Horas
-	$terminoInicio = clone $inicio;
-	$terminoInicio->modify($horarioTermino);
-	$horasInicio = $inicio->diff($terminoInicio);
+	//Caso o início e o Termino sejam no mesmo dia
+	if($totalDias == 0) {
+		$dateInterval = $inicio->diff($termino);
+		$retorno = "";
+		if($dateInterval->y > 0) $retorno .= "{$dateInterval->y} anos, "; 
+		if($dateInterval->m > 0) $retorno .= "{$dateInterval->m} meses, "; 
+		if($dateInterval->d > 0) $retorno .= "{$dateInterval->d} dias, "; 
+		if($dateInterval->h > 0) $retorno .= "{$dateInterval->h} horas, ";
+		$retorno .= "{$dateInterval->m} minutos"; 
+		return $retorno;
+	}
 
-	$terminoTermino = clone $termino;
-	$terminoTermino->modify($horarioInicial);
-	$horasTermino = $termino->diff($terminoTermino);
+	//Calcula as Horas Úteis do Primeiro Dia caso Dia útil
+	if (in_array($inicio->format('N'), $diasUteis)) {
+		$terminoInicio = clone $inicio;
+		$terminoInicio->modify($horarioTermino);
+		$horasInicio = $inicio->diff($terminoInicio);
+	}
 
-	$totalDias = ( $dias -1 ) + $horasInicio->d + $horasTermino->d;
-	$totalHoras = $horasInicio->h + $horasTermino->h;
-	$totalMinutos = $horasInicio->i + $horasTermino->i;
+	//Calcula as Horas Úteis do Último Dia caso Dia Útil
+	if (in_array($termino->format('N'), $diasUteis)) {
+		$terminoTermino = clone $termino;
+		$terminoTermino->modify($horarioInicial);
+		$horasTermino = $termino->diff($terminoTermino);
+	}
 
-	return "{$totalDias} dias, {$totalHoras} Horas e {$totalMinutos} Minutos";
+	$totalHoras = 0;
+	$totalMinutos = 0;
+	$totalSegundos = 0;
+
+	if(isset($horasInicio)) {
+		$totalDias += $horasInicio->d;
+		$totalHoras += $horasInicio->h;
+		$totalMinutos += $horasInicio->i;
+		$totalSegundos += $horasInicio->s;
+	}
+	if(isset($horasTermino)) {
+		$totalDias += $horasTermino->d;
+		$totalHoras += $horasTermino->h;
+		$totalMinutos += $horasTermino->i;
+		$totalSegundos += $horasTermino->s;
+	}	
+
+	//Tratamento para formato 24h
+	if($totalSegundos > 60) {
+		$totalMinutos += floor( $totalSegundos / 60 );
+		$totalSegundos = $totalSegundos % 60;
+	}
+
+	if($totalMinutos > 60) {
+		$totalHoras += floor( $totalMinutos / 60 );
+		$totalMinutos = $totalMinutos % 60;
+	}
+
+	if($totalHoras > 8) {
+		$totalDias += floor( $totalHoras / 8 );
+		$totalHoras = $totalHoras % 8;
+	}
+
+	$dateInterval = new DateInterval("P{$totalDias}DT{$totalHoras}H{$totalMinutos}M");
+
+	$retorno = "";
+	if($dateInterval->y > 0) $retorno .= "{$dateInterval->y} anos, "; 
+	if($dateInterval->m > 0) $retorno .= "{$dateInterval->m} meses, "; 
+	if($dateInterval->d > 0) $retorno .= "{$dateInterval->d} dias, "; 
+	if($dateInterval->h > 0) $retorno .= "{$dateInterval->h} horas, ";
+	$retorno .= "{$dateInterval->m} minutos"; 
+	return $retorno;
 }
